f7599c34f52c81cac2692aa2e848e7867b02c77f
Stud. Filippo Piumatti <f.piumatti.1365@vallauri.net>
2021-05-26 09:59:13 +0200

    implemented first exercise wth ia

---
+++ 030_TaxiFarePrediction/30_TaxiFarePrediction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace _30_TaxiFarePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
